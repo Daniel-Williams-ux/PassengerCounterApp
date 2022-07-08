d19937ecea366b0b3fd3c12cee410f8303a670c5
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-08 14:39:01 +0000

    added a save function

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-let count = 0;
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el");
+let count = 0;
 console.log(countEl)
 
 function increment() {
     count += 1;
     countEl.innerText = count;
+}
+
+function save() {
+    let countStr = count + " - "
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
 }
