537948c428925b718bb8f3b5d3a231c8655037b0
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-08 14:03:14 +0000

    document.getElement

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// document.getElementById("count-el").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
+
 let count = 0;
-  function increment() {
+let countEl = document.getElementById("count-el");
+console.log(countEl)
+
+function increment() {
     count = count + 1;
-    console.log(count);
+    countEl.innerText = count;
 }
