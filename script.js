e3b9ccb92649b9abf1d33200a98247263db8687d
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-08 15:01:21 +0000

    .textContent reads text better including spaces

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ console.log(countEl)
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
     let countStr = count + " - "
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    saveEl.textContent += countStr;
+    console.log(count);
 }
+// Google:
+// innerText alternative mdn
